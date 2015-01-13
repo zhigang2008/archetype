24250f2e1a255b08cbfaf89e1ab5de932f57750b
wangzhigang <wangzhigang@htffund.com>
2015-01-13 13:32:22 +0800

    更新commit

---
+++ sdk-base/src/main/java/com/htffund/etrade/sdk/util/MapUtils.java
@@ -0,0 +1,19 @@
+package com.htffund.etrade.sdk.util;
+
+import java.util.Map;
+/**
+ * @author badqiu
+ */
+public class MapUtils {
+	@SuppressWarnings("all")
+	public static void putIfNull(Map map,Object key,Object defaultValue) {
+		if(key == null)
+			throw new IllegalArgumentException("key must be not null");
+		if(map == null)
+			throw new IllegalArgumentException("map must be not null");
+		if(map.get(key) == null) {
+			map.put(key, defaultValue);
+		}
+	}
+	
+}
---
+++ sdk-base/src/main/java/com/htffund/etrade/sdk/util/UUIDGenerator.java
@@ -0,0 +1,21 @@
+package com.htffund.etrade.sdk.util;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class UUIDGenerator  {
+	public static String getID() {
+		return StringUtils.remove(UUID.randomUUID().toString(), '-');
+	}
+
+	public static void main(String[] args) {
+		UUID.randomUUID();
+		long time = System.currentTimeMillis();
+		for (int i = 0; i < 100000; i++) {
+			UUID.randomUUID();
+		}
+		time = System.currentTimeMillis() - time;
+		System.out.println(time);
+	}
+}
