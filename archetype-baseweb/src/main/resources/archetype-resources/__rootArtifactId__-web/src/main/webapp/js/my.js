166df447c19f9678ec8ab5df69623d57380d456d
wangzhigang <wangzhigang@htffund.com>
2015-02-02 15:33:45 +0800

    更新架构

--- archetype-baseweb/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/webapp/js/my.js
+++ archetype-baseweb/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/webapp/js/my.js
@@ -1,5 +1,6 @@
 /*个性化的javascript */
-
+/*扩展ie的indexOf方法*/
+if(!Array.indexOf){Array.prototype.indexOf = function(obj){for(var i=0; i<this.length; i++){if(this[i]==obj){return i;}} return -1;}};
 /*分页跳转*/
 function goToPage(page,limit,queryForm){
 	var form=$("#"+queryForm);
