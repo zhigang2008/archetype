5b1a1bdc61a06ac8d55553f1c52624bd7c59fa91
wangzhigang <wangzhigang@htffund.com>
2015-02-05 21:58:15 +0800

    更改了uuid

--- sdk-base/src/main/java/com/htffund/etrade/sdk/util/UUIDGenerator.java
+++ sdk-base/src/main/java/com/htffund/etrade/sdk/util/UUIDGenerator.java
@@ -1,6 +1,8 @@
 package com.htffund.etrade.sdk.util;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
+import org.apache.commons.codec.binary.Base64;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -9,6 +11,21 @@ public class UUIDGenerator  {
 		return StringUtils.remove(UUID.randomUUID().toString(), '-');
 	}
 
+	/**简化的ID
+	 * @return 指定长度的ID
+	 */
+	public  static String getShortID(int length){
+		UUID uuid = UUID.randomUUID();
+
+        long lsb = uuid.getLeastSignificantBits();
+        long msb = uuid.getMostSignificantBits();
+
+        byte[] uuidBytes = ByteBuffer.allocate(16).putLong(msb).putLong(lsb).array();
+
+        // Strip down the '==' at the end and make it url friendly   
+        return Base64.encodeBase64URLSafeString(uuidBytes).substring(0, length);
+	}
+	
 	public static void main(String[] args) {
 		UUID.randomUUID();
 		long time = System.currentTimeMillis();
