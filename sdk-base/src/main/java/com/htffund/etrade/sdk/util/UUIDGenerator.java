248c006aa1b82e241279f048ec987fc1f59a05c2
wangzhigang <wangzhigang@htffund.com>
2015-03-05 16:17:42 +0800

    uuid change

--- sdk-base/src/main/java/com/htffund/etrade/sdk/util/UUIDGenerator.java
+++ sdk-base/src/main/java/com/htffund/etrade/sdk/util/UUIDGenerator.java
@@ -1,9 +1,10 @@
 package com.htffund.etrade.sdk.util;
 
 import java.nio.ByteBuffer;
+import java.util.HashMap;
 import java.util.UUID;
-import org.apache.commons.codec.binary.Base64;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 
 public class UUIDGenerator  {
@@ -22,15 +23,29 @@ public class UUIDGenerator  {
 
         byte[] uuidBytes = ByteBuffer.allocate(16).putLong(msb).putLong(lsb).array();
 
-        // Strip down the '==' at the end and make it url friendly   
-        return Base64.encodeBase64URLSafeString(uuidBytes).substring(0, length);
+        // Strip down the '==' at the end and make it url friendly
+        //改为自建方法,resin自带版本过低,无encodeBase64URLSafeString
+        //return Base64.encodeBase64URLSafeString(uuidBytes).substring(0, length);
+        String newStr=new String(Base64.encodeBase64(uuidBytes));
+        newStr=newStr.replace("/", "_").replace("+", "-");
+         
+        if(newStr.length()<length){
+        	length=newStr.length();
+        }
+        
+        return newStr.substring(0, length);
 	}
 	
 	public static void main(String[] args) {
 		UUID.randomUUID();
+		HashMap map =new HashMap();
 		long time = System.currentTimeMillis();
-		for (int i = 0; i < 100000; i++) {
-			UUID.randomUUID();
+		for (int i = 0; i < 1000000; i++) {
+			String id=UUIDGenerator.getShortID(16);
+			if(map.containsKey(id)){
+				System.out.println(id);
+			}
+			map.put(id, id);
 		}
 		time = System.currentTimeMillis() - time;
 		System.out.println(time);
